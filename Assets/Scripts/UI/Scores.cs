b1ee2968b6abe88749cac9e3d85379207accee2e
Andrey Khorkov <and.khorkov@gmail.com>
2018-06-11 13:23:25 -0700

    replaced hardcoded 60 seconds a minute with constant

--- Assets/Scripts/UI/Scores.cs
+++ Assets/Scripts/UI/Scores.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class Scores : BattleElement
 {
+    private const int SECONDS_IN_MIN = 60;
+
     [SerializeField] private Text numSimple;
     [SerializeField] private Text numAdvanced;
     [SerializeField] private Text timer;
@@ -41,8 +43,8 @@ public class Scores : BattleElement
         sb.Remove(0, sb.Length);
 
         var t = args.Seconds;
-        var min =  t/60;
-        var sec = t%60;
+        var min =  t/SECONDS_IN_MIN;
+        var sec = t%SECONDS_IN_MIN;
 
         sb.AppendFormat("{0:D2}:{1:D2}", min, sec);
 
